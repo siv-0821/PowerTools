a7c0269fa0dee2f88eb43788293075b7155658da
siv-0821 <sivasankar11237@gmail.com>
2024-03-18 14:26:48 +0530

    changes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,19 +4,20 @@ import './Home.css';
 import Navbar from '../Navbar/Navbar';
 import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Overview from '../Overview/Overview';
 
 function Home() {
   const { setShowNavbar } = useContext(DataContext);
 
   // Assuming you have some data for multiple cards
   const cardData = [
-    { id: 1, title: 'Card 1', image: 'https://source.unsplash.com/random', description: 'Description for Card 1' },
-    { id: 2, title: 'Card 2', image: 'https://source.unsplash.com/random', description: 'Description for Card 2' },
-    { id: 3, title: 'Card 2', image: 'https://source.unsplash.com/random', description: 'Description for Card 2' },
-    { id: 4, title: 'Card 2', image: 'https://source.unsplash.com/random', description: 'Description for Card 2' },
-    { id: 5, title: 'Card 2', image: 'https://source.unsplash.com/random', description: 'Description for Card 2' },
-    { id: 6, title: 'Card 2', image: 'https://source.unsplash.com/random', description: 'Description for Card 2' },
-    { id: 7, title: 'Card 3', image: 'https://source.unsplash.com/random', description: 'Description for Card 3' }
+    { id: 1, title: 'Card 1', image: 'https://source.unsplash.com/random', desc: 'Description for Card 1' },
+    { id: 2, title: 'Card 2', image: 'https://source.unsplash.com/random', desc: 'Description for Card 2' },
+    { id: 3, title: 'Card 2', image: 'https://source.unsplash.com/random', desc: 'Description for Card 2' },
+    { id: 4, title: 'Card 2', image: 'https://source.unsplash.com/random', desc: 'Description for Card 2' },
+    { id: 5, title: 'Card 2', image: 'https://source.unsplash.com/random', desc: 'Description for Card 2' },
+    { id: 6, title: 'Card 2', image: 'https://source.unsplash.com/random', desc: 'Description for Card 2' },
+    { id: 7, title: 'Card 3', image: 'https://source.unsplash.com/random', desc: 'Description for Card 3' }
     // Add more card data as needed
   ];
 
@@ -27,6 +28,7 @@ function Home() {
       <Navbar id="nav" />
       <div className='home-page'>
         <div className='product-card'>
+          <Overview>
           {cardData.map(card => (
             <Card key={card.id}>
               <CardMedia
@@ -37,7 +39,7 @@ function Home() {
               />
               <CardContent>
                 <Typography variant='h5'>{card.title}</Typography>
-                <Typography>{card.description}</Typography>
+                <Typography>{card.desc}</Typography>
               </CardContent>
               <CardActions>
                 <Button variant='small' component={Link} to={`/overview/${card.id}`}>View</Button>
@@ -45,6 +47,7 @@ function Home() {
               </CardActions>
             </Card>
           ))}
+          </Overview>
         </div>
       </div>
     </div>
--- src/Components/Overview/Overview.jsx
+++ src/Components/Overview/Overview.jsx
@@ -1,28 +1,27 @@
 import { Box, Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material"
 import './Overview.css'
-import { useParams } from "react-router-dom"
-function Overview() {
-    const {id}=useParams()
+function Overview(props) {
+
+
     return (
         <div className="over-body">
             <div className="over-head">
                 <Box width="300px">
-                <Card>
-                    <CardMedia
-                    component="img"
-                    height="140"
-                    image="https://source.unsplash.com/random"
-                    alt="overview" />
-                    <CardContent>
-                        <Typography variant="h5" gutterBottom>{id}abc</Typography>
-                        <Typography variant="body2" gutterBottom>Subject to the license terms for the software, you may redistribute the .EXE files (unmodified) listed below. 
-These files can be run as prerequisites during installation.</Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size="small">View</Button>
-                        <Button size="small">Buy</Button>
-                    </CardActions>
-                </Card>
+                    <Card key={props.id}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={props.image}
+                            alt="overview" />
+                        <CardContent>
+                            <Typography variant="h5" gutterBottom></Typography>
+                            <Typography variant="body2" gutterBottom>{props.title}</Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button size="small">View</Button>
+                            <Button size="small">Buy</Button>
+                        </CardActions>
+                    </Card>
                 </Box>
             </div>
         </div>
